2b2053ba57ee35de05ab3f4c8417e9cbf03ab122
pavanjava <manthapavankumar11@gmail.com>
2023-11-28 04:20:52 +0530

    few more array methods

--- tutorial/tutorial/CommandLineArgsDemo.cs
+++ tutorial/tutorial/CommandLineArgsDemo.cs
@@ -16,5 +16,15 @@ public class CommandLineArgsDemo
         String[] data = simpleArrayDemo.Initialize();
         simpleArrayDemo.Display(data, 1);
         
+        // find the index of specific element
+        simpleArrayDemo.SearchForElement(data, "JavaScript");
+
+        // sort the Array elements
+        simpleArrayDemo.SortAndDisplay(data);
+        
+        // check if two arrays are same or not
+        String[] data2 = { "Java", "C#", "Data Science", "JavaScript" };
+        // Array.Sort(data2); uncomment if you want `True`, as our original data is already sorted we have to sort the second array also
+        Console.WriteLine(simpleArrayDemo.CompareArrays(data, data2));
     }
 }
--- tutorial/tutorial/SimpleArrayDemo.cs
+++ tutorial/tutorial/SimpleArrayDemo.cs
@@ -4,7 +4,7 @@ public class SimpleArrayDemo
 {
     public String[] Initialize()
     {
-        String[] data = new[] { "Java", "C#", "Data Science", "JavaScript" };
+        String[] data = { "Java", "C#", "Data Science", "JavaScript" };
         return data;
         
     }
@@ -12,6 +12,25 @@ public class SimpleArrayDemo
     public void Display(String[] data, int index)
     {
         Console.WriteLine(data[index]);
+    }
+
+    public void SearchForElement(String[] data, String value)
+    {
+        Console.WriteLine(Array.IndexOf(data, value));
+    }
+
+    public void SortAndDisplay(String[] data)
+    {
+        Array.Sort(data);
+        foreach (var s in data)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    public bool CompareArrays(String[] data1, String[] data2)
+    {
+        return Enumerable.SequenceEqual(data1, data2);
     }
     
 }
