cb821fd69aca038dab28c6bb713e5845902c697c
pavanjava <manthapavankumar11@gmail.com>
2023-11-29 09:57:33 +0530

    changed the display of loops using for

--- tutorial/tutorial/MultiDimentionalArrays.cs
+++ tutorial/tutorial/MultiDimentionalArrays.cs
@@ -9,18 +9,25 @@ public class MultiDimentionalArrays
         {
             String[,] languages =
             {
-                { "Java", "Java Script" }, 
+                { "Java", "Java Script" },
                 { "Python", "C#" }
             };
-        
-            Console.WriteLine(languages[rowIndex,columnIndex]);
+
+            // display using traditional for loop
+            for (int i = 0; i < languages.GetLength(0); i++)
+            {
+                for (int j = 0; j < languages.GetLength(1); j++)
+                {
+                    Console.Write(languages[i,j]+ " ");        
+                }
+                Console.WriteLine();
+            }
         }
         catch (Exception e) // handled exception for outside the bounds of array
         {
             Console.WriteLine(e);
             throw;
         }
-        
     }
 
     // initialize and display normal jagged array (kind of multi dim array with fixed rows and dynamic number of columns)
@@ -30,13 +37,21 @@ public class MultiDimentionalArrays
         {
             String[][] languages =
             {
-                new []{"Java", "Spring Boot", "JPA"},
-                new []{"Java Script", "ReactJS", "NodeJS", "NestJS", "ExpressJS", "Angular"},
-                new []{"C#", "Blazor", "Entity Framework", "Web API"},
-                new []{"MongoDB", "Cassandra"}
+                new[] { "Java", "Spring Boot", "JPA" },
+                new[] { "Java Script", "ReactJS", "NodeJS", "NestJS", "ExpressJS", "Angular" },
+                new[] { "C#", "Blazor", "Entity Framework", "Web API" },
+                new[] { "MongoDB", "Cassandra" }
             };
-        
-            Console.WriteLine(languages[rowIndex][columnIndex]);
+
+            // display using traditional for loop
+            for (int i = 0; i < languages.Length; i++)
+            {
+                for (int j = 0; j < languages[i].Length; j++)
+                {
+                    Console.Write(languages[i][j]+ " ");        
+                }
+                Console.WriteLine();
+            }
         }
         catch (Exception e) // handled exception for outside the bounds of array
         {
